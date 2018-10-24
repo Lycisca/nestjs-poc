fece347659aba2272d30307acbae38fc00252a25
Miguel Savignano <migue.masx@gmail.com>
2018-10-24 21:36:59 +0200

    add custom definition of database

--- config/database.js
+++ config/database.js
@@ -1,25 +1,49 @@
 const fs = require('fs');
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "password",
-    "database": "database_development",
-    "host": process.env.DB_HOSTNAME || "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    dialect: "mysql",
+    username: "root",
+    password: "password",
+    database: "database_development",
+    host: process.env.DB_HOSTNAME || "127.0.0.1",
+    define: {
+      // underscored: false
+      charset: 'utf8',
+      dialectOptions: {
+        collate: 'utf8_general_ci'
+      },
+      timestamps: true
+    }
   },
-  "test": {
-    "username": "root",
-    "password": "password",
-    "database": "database_test",
-    "host": process.env.DB_HOSTNAME || "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    dialect: "mysql",
+    username: "root",
+    password: "password",
+    database: "database_test",
+    host: process.env.DB_HOSTNAME || "127.0.0.1",
+    define: {
+      // underscored: false
+      charset: 'utf8',
+      dialectOptions: {
+        collate: 'utf8_general_ci'
+      },
+      timestamps: true
+    }
   },
-  "production": {
+  production: {
+    dialect: "mysql",
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
-    "dialect": "mysql"
+    define: {
+      // underscored: false
+      charset: 'utf8',
+      dialectOptions: {
+        collate: 'utf8_general_ci'
+      },
+      timestamps: true
+    }
   }
 }
