2a554ec95c1c4cb7fc5ef15714a7857bedc1b3d7
Nuria <hernandez.glez@gmail.com>
2018-10-24 15:48:45 +0200

    Remove field in dto update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,6 @@
 import { ApiModelProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
-  @ApiModelProperty() readonly id: number;
   @ApiModelProperty() readonly firstName: string;
   @ApiModelProperty() readonly lastName: string;
 }
