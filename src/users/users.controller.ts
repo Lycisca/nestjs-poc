c70b44a38469d91d696b3544141f448b17e11c16
Nuria <hernandez.glez@gmail.com>
2018-10-25 16:06:53 +0200

    New validations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,7 @@ import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ValidationNumber, ValidationPresence } from '../pipes/validation.pipe';
+import { ValidationUser } from '../pipes/validation.pipe';
 
 @Controller('users')
 export class UsersController {
@@ -15,23 +15,25 @@ export class UsersController {
   }
 
   @Get(':id')
-  @UsePipes(new ValidationNumber(), new ValidationPresence())
+  @UsePipes(new ValidationUser())
   async show(@Param('id') userId: number) {
     return this.usersService.show(userId);
   }
 
   @Post()
+  @UsePipes(new ValidationUser())
   async create(@Body() createUserDto: CreateUserDto) {
     await this.usersService.create(createUserDto);
   }
 
   @Put(':id')
-  @UsePipes(new ValidationNumber(), new ValidationPresence())
+  @UsePipes(new ValidationUser())
   async update(@Param('id') userId: number, @Body() updateUserDto: UpdateUserDto) {
     await this.usersService.update(userId, updateUserDto);
   }
 
   @Delete(':id')
+  @UsePipes(new ValidationUser())
   async destroy(@Param('id') userId: number){
     await this.usersService.destroy(userId);
   }
