518bc4b6cbd34f3d8c2bab654d65eb85eb4004ec
Miguel Savignano <migue.masx@gmail.com>
2018-10-26 23:27:16 +0200

    configure HEROKU port in docker image

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,1 +1,2 @@
-docker build -t nestjs-poc .
+HEROKU_APP=vast-brushlands-70321
+docker build -t nestjs-poc -t registry.heroku.com/${HEROKU_APP}/web .
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,15 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-  .setTitle('Users example')
-  .setDescription('The users API description')
-  .setVersion('1.0')
-  .addTag('users')
-  .build();
+    .setTitle('Users example')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  console.log(`Server start in port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
