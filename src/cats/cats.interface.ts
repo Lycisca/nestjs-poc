97aaff88bcd30312eb822f59d20bca12e49a0704
Nuria <hernandez.glez@gmail.com>
2018-10-26 14:18:24 +0200

    Return promises in service

--- src/cats/cats.interface.ts
+++ src/cats/cats.interface.ts
@@ -3,13 +3,14 @@ import { UpdateCatsDto } from "./dto/update-cats-dto";
 
 export interface CatsInterface {
   name: string,
-  age: number
+  age: number,
+  user_id: number
 }
 
 export interface CatsServiceInterface {
-  index(): Array<CatsInterface>
-  show(): CatsInterface
-  create(createCatDto: CreateCatsDto): CatsInterface
-  update(catId: number, updateCatDto: UpdateCatsDto): CatsInterface
-  delete(catId: number): CatsInterface
+  index(): Promise<Array<CatsInterface>>
+  show(): Promise<CatsInterface>
+  create(createCatDto: CreateCatsDto): Promise<CatsInterface>
+  update(catId: number, updateCatDto: UpdateCatsDto): Promise<CatsInterface>
+  delete(catId: number): Promise<CatsInterface>
 }
