ed4b62896387ea66bff40b89ab912aea54916e45
Miguel Savignano <miguel.savignano@the-cocktail.com>
2018-10-28 00:58:26 +0200

    cat service service

---
+++ src/cats/cats.service.spec.ts
@@ -0,0 +1,32 @@
+import { IFindOptions, Model } from 'sequelize-typescript';
+import { CatsService } from './users.service';
+import { CatsServiceInterface } from './cats.interface';
+import { databaseProviders } from '../databases/database.providers';
+
+// class CatMock implements CatsServiceInterface {
+//   static findAll<T extends Model<T>>(
+//     this: new () => T,
+//     options?: IFindOptions<T>,
+//   ): any {
+//     return [{ firstName: 'cat' }];
+//   }
+// }
+
+// const catsServiceFactory = CatRepository => {
+//   // databaseProviders[0].useFactory();
+//   // return new CatsService(CatRepository);
+// };
+
+describe('Users Service', () => {
+  it('users service', async () => {
+    const service = new CatsService();
+    expect(service).toBeInstanceOf(CatsService);
+  });
+
+  // it('test interface', async () => {
+  //   const repo = UserMock;
+  //   const usersService = usersServiceFactory(repo);
+  //   const users = await usersService.index();
+  //   expect(users).toEqual([{ firstName: 'user' }]);
+  // });
+});
