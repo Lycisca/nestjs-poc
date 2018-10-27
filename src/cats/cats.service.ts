66ad5300082daea2f5f4dbb48f0c4593be9dd6e3
Miguel Savignano <miguel.savignano@the-cocktail.com>
2018-10-28 01:47:56 +0200

    implement interface

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,9 +1,27 @@
-export class CatsService {
+import { CatsServiceInterface } from './cats.interface';
+import { CreateCatsDto } from './dto/create-cats.dto';
+import { UpdateCatsDto } from './dto/update-cats-dto';
+
+export class CatsService implements CatsServiceInterface {
   constructor(private usersRepository) {}
+
   index() {
     return this.usersRepository.findAll();
   }
+
   show() {
     return this.usersRepository.findById();
+  }
+
+  async create(createCatDto: CreateCatsDto) {
+    return { name: 'string', age: 1, user_id: 1 };
+  }
+
+  async update(catId: number, updateCatDto: UpdateCatsDto) {
+    return { name: 'string', age: 1, user_id: 1 };
+  }
+
+  async delete(catId: number) {
+    return { name: 'string', age: 1, user_id: 1 };
   }
 }
