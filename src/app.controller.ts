5b398420e1513d82d739d70b46dbddf0d305794a
Miguel Savignano <migue.masx@gmail.com>
2018-10-28 22:42:31 +0100

    http proxy with request pipe

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Res, Response, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+const request = require('request');
 
 @Controller()
 export class AppController {
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   root(): string {
     return 'Hello world!!!!!';
+  }
+
+  @Get('/cats_breeds')
+  async breeds(@Response() response: Response) {
+    request.get('https://catfact.ninja/breeds').pipe(response);
+  }
+
+  @Get('/docs/cats_breeds')
+  // Error with type Response and redirect method
+  async docs(@Response() response) {
+    return response.redirect('https://catfact.ninja/');
   }
 }
