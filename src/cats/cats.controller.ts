c450119d2f5225a6f318aa6e3d3437631dbfa30d
Nuria <hernandez.glez@gmail.com>
2018-10-30 08:42:37 +0100

    Remove validation from controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -12,7 +12,6 @@ import {
 // import { Cat } from './cat.entity';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat-dto';
-// import { ValidationCat } from '../pipes/validation.pipe';
 import { CatsServiceInterface } from './cats.interface';
 
 @Controller('cats')
@@ -25,25 +24,21 @@ export class CatsController {
   }
 
   @Get(':id')
-  // @UsePipes(new ValidationUser())
   async show(@Param('id') CatId: number) {
     return this.catsService.show(CatId);
   }
 
   @Post()
-  // @UsePipes(new ValidationUser())
   async create(@Body() createCatDto: CreateCatDto) {
     return this.catsService.create(createCatDto);
   }
 
   @Put(':id')
-  // @UsePipes(new ValidationUser())
   async update(@Param('id') CatId: number, @Body() updateCatDto: UpdateCatDto) {
     return this.catsService.update(CatId, updateCatDto);
   }
 
   @Delete(':id')
-  // @UsePipes(new ValidationUser())
   async delete(@Param('id') CatId: number) {
     return this.catsService.delete(CatId);
   }
