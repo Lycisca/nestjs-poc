e3a74a4e4b06ebc7fc47c31dff9db32dfb583587
Miguel Savignano <migue.masx@gmail.com>
2018-10-30 02:07:52 +0100

    add merge params and heraders with axios request

--- src/decorators/HttpProxy.ts
+++ src/decorators/HttpProxy.ts
@@ -2,22 +2,17 @@ const axios = require('axios');
 import { Query } from '@nestjs/common';
 
 class HttpProxyController {
-  constructor(
-    private originalFnc,
-    private url,
-    private headers = {},
-    private params = {},
-  ) {}
+  constructor(private originalFnc, private url, private options: any = {}) {}
 
   perform(@Query() query) {
     const url = this.url;
-    const headers = this.headers;
-    const params = this.params;
-    console.log('HttpProxy to: ', url);
+    const headers = this.options.headers;
+    const params = this.options.params;
+    console.log('HttpProxy to: ', url, params);
     // this.originalFnc.apply(this, args);
     return new Promise((resolve, reject) => {
       axios
-        .get(url, { params: { ...query, ...params }, headers })
+        .get(url, { params: { ...params, ...query }, headers })
         .then(response => {
           resolve(response.data);
         })
@@ -26,16 +21,15 @@ class HttpProxyController {
   }
 }
 
-// url: url to fetch json
-// params: params merged with query arguments
-// headers: custom headers
-export const HttpProxy = (url: string, params: any = {}, headers: any = {}) => (
-  target,
-  name,
-  descriptor,
-) => {
+// url: Url to fetch json
+// params: Params merged with query arguments, query from request low priority
+// headers: Custom headers
+export const HttpProxy = (
+  url: string,
+  options: { params?: any; headers?: any } = {},
+) => (target, name, descriptor) => {
   const original = descriptor.value;
-  const proxy = new HttpProxyController(original, url);
+  const proxy = new HttpProxyController(original, url, options);
   descriptor.value = descriptor.value = proxy.perform.bind(proxy);
 
   return descriptor;
