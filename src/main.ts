ab24a4260a3d0aa9c1c2dac94cef0cc6de168c1b
Miguel Savignano <migue.masx@gmail.com>
2018-11-02 01:46:52 +0100

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   const options = new DocumentBuilder()
     .setTitle('Users example')
     .setDescription('The users API description')
