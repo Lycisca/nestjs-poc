fc47b7e8f090cefef8b2462345012a244db3c2b9
Miguel Savignano <migue.masx@gmail.com>
2018-11-09 18:12:09 +0100

    graphql test

--- src/databases/database.providers.ts
+++ src/databases/database.providers.ts
@@ -4,15 +4,17 @@ import { Cat } from '../cats/cat.entity';
 import DatabaseConfig from '../config/database';
 
 const env = process.env.NODE_ENV || 'development';
+export const sequelizeInit = () => {
+  const databaseConfig = DatabaseConfig[env];
+  const sequelize = new Sequelize(databaseConfig);
+  sequelize.addModels([User, Cat]);
+  // await sequelize.sync(); // Don't create tables to start application
+  return sequelize;
+};
+
 export const databaseProviders = [
   {
     provide: 'SequelizeToken',
-    useFactory: () => {
-      const databaseConfig = DatabaseConfig[env];
-      const sequelize = new Sequelize(databaseConfig);
-      sequelize.addModels([User, Cat]);
-      // await sequelize.sync(); // Don't create tables to start application
-      return sequelize;
-    },
+    useFactory: () => sequelizeInit,
   },
 ];
--- test/graphql.e2e-spec.ts
+++ test/graphql.e2e-spec.ts
@@ -2,44 +2,82 @@ import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { sequelizeInit } from '../src/databases/database.providers';
+import { throws } from 'assert';
+
 const dotenv = require('dotenv');
 dotenv.config({ path: '.env.test' });
 
 const createApp = async () => {
-  let app: INestApplication;
   const moduleFixture = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
 
-  app = moduleFixture.createNestApplication();
+  const app: INestApplication = moduleFixture.createNestApplication();
   return app.init();
 };
 
-const graphqlRequest = (app, query, variables = {}) => {
-  return request(app.getHttpServer())
+const graphqlRequest = async (app, query, variables = {}) => {
+  const { body } = await request(app.getHttpServer())
     .post('/graphql')
     .send({ query, variables })
     .set('Accept', 'application/json');
+  if (body && body.errors && body.errors.length) {
+    console.error('query error', query);
+    console.error(body);
+    throw new Error('invalid query');
+  }
+  return body;
 };
 
-const createUserByHttp = (app, userFixture = undefined) =>
-  request(app.getHttpServer())
-    .post('/users')
-    .send(userFixture || { firstName: 'John', lastName: 'Doe' })
-    .set('Accept', 'application/json')
-    .expect(201);
+const sequelize = sequelizeInit();
+
+const truncateAll = sequelize => {
+  Object.values(sequelize.models).map((model: any) =>
+    model.destroy({ where: {}, truncate: true }),
+  );
+};
+
+const createUserByHttp = async (app, userFixture = undefined) => {
+  const input = userFixture || { firstName: 'John', lastName: 'Doe' };
+  const query = `mutation($input: UserCreateInput!) {
+    userCreate(input: $input) {
+      id
+      firstName
+      lastName
+    }
+  }`;
+  const { data } = await graphqlRequest(app, query, { input });
+  return data.userCreate;
+};
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
+    truncateAll(sequelize);
     app = await createApp();
   });
 
   it('QUERY { users { id } }', async () => {
-    const response = await graphqlRequest(app, 'query { users { id } }').expect(
-      200,
+    const response = await graphqlRequest(app, 'query { users { id } }');
+    expect(response.data.users).toBeInstanceOf(Array);
+  });
+
+  it('QUERY { user(id: $id) { id } }', async () => {
+    const user = await createUserByHttp(app);
+    const { data } = await graphqlRequest(
+      app,
+      `query($id: ID!) {
+        user(id: $id) {
+          id
+          firstName
+          lastName
+        }
+      }`,
+      { id: user.id },
     );
-    expect(response.body.data.users).toBeInstanceOf(Array);
+    console.log('USER', data);
+    // expect(data.user).toHaveProperty('id');
   });
 });
