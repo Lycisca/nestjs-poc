644e57fc8b536527bd7ed9b74422c1b860d17711
Miguel Savignano <migue.masx@gmail.com>
2018-11-10 04:58:54 +0100

    perform now return promise
    use jest in package
    
    checksum package-lock.json
    
    no use global jest?

--- src/config/database.js
+++ src/config/database.js
@@ -1,49 +1,52 @@
 /* tslint:disable */
 // import fs from 'fs';
-// import {
-//   ISequelizeConfig
-// } from 'sequelize-typescript';
+// import { ISequelizeConfig } from 'sequelize-typescript';
 
 // interface DatabaseConfig {
 //   development: ISequelizeConfig;
 //   test: ISequelizeConfig;
 //   production: ISequelizeConfig;
 // }
-const fs = require('fs');
 
 const config = {
   development: {
-    dialect: "mysql",
-    username: "root",
-    password: "password",
-    database: "database_development",
-    host: process.env.DB_HOSTNAME || "127.0.0.1",
+    dialect: 'mysql',
+    username: 'root',
+    password: 'password',
+    database: 'database_development',
+    host: process.env.DB_HOSTNAME || '127.0.0.1',
     port: 3306,
     define: {
       // underscored: false
       charset: 'utf8',
-      dialectOptions: {
-        collate: 'utf8_general_ci'
-      },
-      timestamps: true
-    }
+      // dialectOptions: {
+      //   collate: 'utf8_general_ci',
+      // },
+      timestamps: true,
+    },
   },
   test: {
-    dialect: "sqlite",
+    dialect: 'sqlite',
+    // key not need for sqlite
+    database: 'database_test',
+    username: 'root',
+    password: 'password',
+    // key not need for sqlite
     // the storage engine for sqlite
     // - default ':memory:'
     storage: 'db/database.sqlite',
+    logging: false,
     define: {
       // underscored: false
       charset: 'utf8',
-      dialectOptions: {
-        collate: 'utf8_general_ci'
-      },
-      timestamps: true
-    }
+      // dialectOptions: {
+      //   collate: 'utf8_general_ci',
+      // },
+      timestamps: true,
+    },
   },
   production: {
-    dialect: "mysql",
+    dialect: 'mysql',
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
@@ -52,13 +55,13 @@ const config = {
     define: {
       // underscored: false
       charset: 'utf8',
-      dialectOptions: {
-        collate: 'utf8_general_ci'
-      },
-      timestamps: true
-    }
-  }
-}
-module.exports = config
-
-module.exports.default = config
+      // dialectOptions: {
+      //   collate: 'utf8_general_ci',
+      // },
+      timestamps: true,
+    },
+  },
+};
+module.exports = config;
+module.exports.default = config;
+// export default config;
--- src/jobs/application.job.ts
+++ src/jobs/application.job.ts
@@ -1,18 +1,41 @@
+import { Job, Queue } from 'kue';
 export class JobProvider {
-  create: (...args: any[]) => { save: () => {} };
+  create(...args: any[]) {
+    const saveFunction = () => ({
+      on: (..._args: any[]) => saveFunction,
+    });
+    return { save: saveFunction };
+  }
   process: (...args: any[]) => {};
 }
+
 export class ApplicationJob {
   // event name
   event: string;
 
-  constructor(public readonly jobProvider: JobProvider) {
+  constructor(public readonly jobProvider: Queue) {
     this.event = this.constructor.name;
   }
 
   // publish to redis queue
   performLater(data) {
     this.jobProvider.create(this.event, data).save();
+  }
+
+  performNow(data) {
+    return new Promise((resolve, reject) => {
+      const job: Job = this.jobProvider.create(this.event, data).save();
+      job
+        .on('complete', result => {
+          resolve(result);
+        })
+        .on('failed attempt', (errorMessage, doneAttempts) => {
+          reject({ errorMessage, doneAttempts });
+        })
+        .on('failed', errorMessage => {
+          reject({ errorMessage });
+        });
+    });
   }
 
   // subscribe to redis queue
