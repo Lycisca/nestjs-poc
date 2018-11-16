877beb42d2f6158889a87737d0f7c2df0e9f16c1
Miguel Savignano <miguel.savignano@the-cocktail.com>
2018-11-16 17:54:29 +0100

    use only render function email templates

--- src/mailer/welcome.email.ts
+++ src/mailer/welcome.email.ts
@@ -1,10 +1,23 @@
 import { sendEmail } from './application.mailer';
+import { createTransporter } from './application.mailer';
+const path = require('path');
+const Email = require('email-templates');
 
 export const WelcomeEmail = transporter => ({ to, name }) => {
-  sendEmail(transporter)({
+  welcomeEmailTemplate({ to, name }).then(html => {
+    sendEmail(transporter)({
+      to,
+      subject: 'Hello ✔',
+      text: '',
+      html,
+    });
+  });
+};
+
+const welcomeEmailTemplate = async ({ to, name }) => {
+  const email = new Email();
+  return email.render(path.join(__dirname, 'emails', 'welcome', 'html'), {
     to,
-    text: `Welcome ${name}`,
-    subject: 'Hello ✔',
-    html: `<b>Welcome ${name}</b>`,
+    name,
   });
 };
